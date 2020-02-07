a4142d3b57bbeb30305a1893add1f892ca0e450c
Dingqing Yang <williamyang1203@gmail.com>
2020-02-07 12:39:40 -0800

    add annealer, need further hyperparameter tuning

--- placement.py
+++ placement.py
@@ -10,10 +10,17 @@ COLORS = ['black', 'red', 'yellow', 'azure4', 'orange', 'maroon', 'pink', 'lime 
 
 
 class Placer:
+    '''
+    self.nets: List (net ID -> list of block IDs)
+    self.blocks: List (block ID -> list of net IDs)
+    self.state: np 2D array (coordinates -> block ID)
+    self.block_to_coordinates: np array of shape (num_blocks * 2)
+    '''
+
     def __init__(self, infile, verbose=False):
         self.verbose = verbose
-        self.grid_size, self.num_cells, self.num_nets, self.nets = parseInput(infile)
-        self.blocks = block2Net(self.num_cells, self.nets)
+        self.grid_size, self.num_blocks, self.num_nets, self.nets = parseInput(infile)
+        self.blocks = block2Net(self.num_blocks, self.nets)
         self.cost = 0
 
         self.startGUI()
@@ -70,7 +77,7 @@ class Placer:
     def randomPlacement(self):
         ''' random placement'''
         # place all blocks in order
-        ordered = np.concatenate([np.arange(self.num_cells), -1 * np.ones(np.prod(self.grid_size) - self.num_cells, dtype=np.int32)])
+        ordered = np.concatenate([np.arange(self.num_blocks), -1 * np.ones(np.prod(self.grid_size) - self.num_blocks, dtype=np.int32)])
         # random shuffling
         self.state = np.random.permutation(ordered).reshape(self.grid_size)
         # print(self.state)
@@ -84,25 +91,113 @@ class Placer:
         cost = np.zeros(self.num_nets, dtype=np.int32)
         for i in range(self.num_nets):
             # print(self.nets[i])
-            cost[i] = self.evalCost(self.block_to_coordinates[self.nets[i], :])
+            cost[i] = evalCost(self.block_to_coordinates[self.nets[i], :])
         # print('cost of each net', cost)
         # print('current total cost', np.sum(cost))
         return cost
 
-    def evalCost(self, input):
-        min = input.min(axis=0)
-        max = input.max(axis=0)
-        return np.sum(max - min)
+    def evalDeltaCost(self, pair):
+        '''
+        Get Bounding Box for untouched blocks min_other, max_other
+        Compare cost between stacked(min_other, max_other, new) and stacked(min_other, max_other, old)
+        BTW, is the old net cost available?
+        '''
+        # print('state:')
+        # print(self.state)
+        block_id1, block_id2 = pair
+        proposed_cost = np.zeros(self.num_nets, dtype=np.int32)
+        delta = 0
+        # print('block1 id', block_id1)
+        for net_id in self.blocks[block_id1]:
+            # skip delta evalution if both block belong to this net
+            if block_id2 not in self.nets[net_id]:
+                proposed = [block if block != block_id1 else block_id2 for block in self.nets[net_id]]
+                # print('proposed blocks')
+                # print(proposed)
+                # min_other, max_other = getBBox(self.block_to_coordinates[other_blocks, :])
+                # TODO: prune if 2 block belong to the same net
+                proposed_cost[net_id] = evalCost(self.block_to_coordinates[proposed, :])
+                delta += proposed_cost[net_id] - self.cost[net_id]
+                # print('proposed_cost for net {} is {}, current cost is {}, current delta is {}'.format(net_id, proposed_cost[net_id], self.cost[net_id], delta))
+
+        # print('block2 id', block_id2)
+        for net_id in self.blocks[block_id2]:
+            # avoid count the same net again
+            if net_id not in self.blocks[block_id1]:
+                proposed = [block if block != block_id2 else block_id1 for block in self.nets[net_id]]
+                # print('proposed blocks')
+                # print(proposed)
+                # min_other, max_other = getBBox(self.block_to_coordinates[other_blocks, :])
+                # TODO: prune evaluated nets?
+                proposed_cost[net_id] = evalCost(self.block_to_coordinates[proposed, :])
+                delta += proposed_cost[net_id] - self.cost[net_id]
+                # print('proposed_cost for net {} is {}, current cost is {}, current delta is {}'.format(net_id, proposed_cost[net_id], self.cost[net_id], delta))
+        # print('debug')
+        # print('proposed_cost', proposed_cost)
+        # print('cost', self.cost)
+        # print('selected cost', self.cost[proposed_cost > 0])
+        # print('sum selected cost', np.sum(self.cost[proposed_cost > 0]))
+        # print('delta', delta)
+        # print((np.sum(proposed_cost) - np.sum(self.cost[proposed_cost > 0]) - delta))
+        assert((np.sum(proposed_cost) - np.sum(self.cost[proposed_cost > 0]) - delta) < epsilon)
+        return proposed_cost, delta
 
     def block2Coordinates(self):
         y_bound, x_bound = self.grid_size
-        self.block_to_coordinates = np.zeros((self.num_cells, 2))
+        self.block_to_coordinates = np.zeros((self.num_blocks, 2), dtype=np.int32)
 
         for i in range(y_bound):
             for j in range(x_bound):
                 current_cell = self.state[i, j]
                 if current_cell != -1:
                     self.block_to_coordinates[current_cell] = (i, j)
+
+    def simulatedAnnealer(self, init_temperature=1000, cooling_period=100, max_iter=2000000, beta=0.9):
+        '''
+        init_temperature: initial temperature
+        cooling_period: num of iterations to decrease the temperature
+        beta: cooling coefficient
+        '''
+        temp = init_temperature
+        num_iter = 0
+        while True:
+            for _ in range(cooling_period):
+                # swap 2 random cooredinate
+                pair_to_swap = tuple(np.random.choice(self.num_blocks, 2, replace=False))
+
+                proposed_cost, d_cost = self.evalDeltaCost(pair_to_swap)
+
+                r = np.random.uniform()
+
+                if r < np.exp(-d_cost / temp):
+                    # swap
+                    bid1, bid2 = pair_to_swap
+                    b1_coords = tuple(self.block_to_coordinates[bid1])
+                    b2_coords = tuple(self.block_to_coordinates[bid2])
+                    # print(bid1)
+                    # print(type(self.block_to_coordinates[bid1]))
+                    # update self.state and self.block_to_coordinates
+                    # print('before state swap')
+                    # print(self.state)
+                    self.state[b1_coords], self.state[b2_coords] = bid2, bid1
+                    # print('after state swap')
+                    # print(self.state)
+                    # print('before block 2 coordinates')
+                    # print(self.block_to_coordinates)
+                    self.block_to_coordinates[[bid1, bid2]] = self.block_to_coordinates[[bid2, bid1]]
+                    # print('after block 2 coordinates')
+                    # print(self.block_to_coordinates)
+                    # print('before cost')
+                    # print(self.cost)
+                    self.cost[proposed_cost > 0] = proposed_cost[proposed_cost > 0]
+                    # print('after cost')
+                    # print(self.cost)
+            temp *= beta
+            num_iter += cooling_period
+            print('current iteration {}, current cost {}'.format(num_iter, np.sum(self.cost)))
+            if num_iter > max_iter:
+                print('Reach maximum number of iterations, exit!')
+                break
 
 
 if __name__ == '__main__':
@@ -111,4 +206,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     placer = Placer(args.infile)
+    placer.simulatedAnnealer()
+    placer.plot()
     placer.root.mainloop()
--- utils.py
+++ utils.py
@@ -2,23 +2,26 @@ import argparse
 import numpy as np
 
 
+epsilon = 1e-4
+
+
 def parseInput(infile):
     '''
     Take an input file, parse the grid size, total number of cells,
     and generate a net2block mapping (i.e. net ID -> block IDs)
     '''
     with open(infile, 'r') as f:
-        num_cells, num_nets, ny, nx = tuple([int(x) for x in f.readline().rstrip('\n').split(' ')])
-        print(num_cells)
+        num_blocks, num_nets, ny, nx = tuple([int(x) for x in f.readline().rstrip('\n').split(' ')])
+        print(num_blocks)
         print(num_nets)
         print(ny)
         print(nx)
         nets = []
         for _ in range(num_nets):
             f.readline()  # skip empty line
-            nets.append(tuple([int(cell) for i, cell in enumerate(f.readline().rstrip('\n').split(' ')) if cell != '' and i != 0]))
+            nets.append([int(cell) for i, cell in enumerate(f.readline().rstrip('\n').split(' ')) if cell != '' and i != 0])
 
-        return (ny, nx), num_cells, num_nets, nets
+        return (ny, nx), num_blocks, num_nets, nets
 
 
 def block2Net(num_blocks, nets):
@@ -31,25 +34,43 @@ def block2Net(num_blocks, nets):
         for j in range(len(nets)):
             if i in nets[j]:
                 block.append(j)
-        blocks.append(tuple(block))
+        blocks.append(block)
     return blocks
 
 
-def evalDeltaCost(old, new):
-    '''
-    old on the boundary, reevaluate the boundary
-    else, see if new become new boundary
-    '''
-    old_min = old.min(axis=0)
-    old_max = old.max(axis=0)
-    print('old min: {}, old max: {}'.format(old_min, old_max))
-    new_min = new.min(axis=0)
-    new_max = new.max(axis=0)
-    print('new min: {}, new max: {}'.format(new_min, new_max))
+# def evalDeltaCost(old, new):
+#     '''
+#     old on the boundary, reevaluate the boundary
+#     else, see if new become new boundary
+#     '''
+#     old_min = old.min(axis=0)
+#     old_max = old.max(axis=0)
+#     print('old min: {}, old max: {}'.format(old_min, old_max))
+#     new_min = new.min(axis=0)
+#     new_max = new.max(axis=0)
+#     print('new min: {}, new max: {}'.format(new_min, new_max))
 
-    old_cost = np.sum(old_max - old_min)
-    new_cost = np.sum(new_max - new_min)
-    return new_cost - old_cost
+#     old_cost = np.sum(old_max - old_min)
+#     new_cost = np.sum(new_max - new_min)
+#     return new_cost - old_cost
+
+def getBBox(input):
+    '''
+    Evaluate half-perimeter of the smallest bounding box of n coordinate
+    input: a numpy array with size n by 2
+    '''
+    min = input.min(axis=0)
+    max = input.max(axis=0)
+    return min, max
+
+
+def evalCost(input):
+    '''
+    Evaluate half-perimeter of the smallest bounding box of n coordinate
+    input: a numpy array with size n by 2
+    '''
+    min, max = getBBox(input)
+    return np.sum(max - min)
 
 
 def betterEvalDeltaCost(old, new):
@@ -64,14 +85,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='CPEN 513 Assignment 2: Placement')
     parser.add_argument('--infile', '-i', default='benchmarks/cm138a.txt', help='input file')
     args = parser.parse_args()
-    grid_size, num_cells, num_nets, nets = parseInput(args.infile)
+    grid_size, num_blocks, num_nets, nets = parseInput(args.infile)
     print('grid size: ', grid_size)
-    print('num_cells: ', num_cells)
+    print('num_blocks: ', num_blocks)
     print('nets: ', nets)
-    blocks = block2Net(num_cells, nets)
+    blocks = block2Net(num_blocks, nets)
     print('blocks: ', blocks)
-
-    old = np.array([[0, 1], [1, 3], [2, 5], [6, 3]])
-    new = np.array([[0, 1], [1, 3], [3, 0], [6, 3]])
-    print('test delta', evalDeltaCost(old, new))
-    # print('total L1', totalL1Distance([(3, 4), (1, 2), (5, 6)]))
